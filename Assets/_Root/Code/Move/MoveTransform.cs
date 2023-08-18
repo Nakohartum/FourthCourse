0ab0e6740d03fda5459a3d5d307456175689f879
Nakohartum <artemofgta@gmail.com>
2023-08-18 15:06:29 +0500

    First lesson

---
+++ Assets/_Root/Code/Move/MoveTransform.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class MoveTransform : IMove
+    {
+        private readonly Transform _transform;
+        public float Speed { get; protected set; }
+        private Vector3 _move;
+
+        public MoveTransform(Transform transform, float speed)
+        {
+            _transform = transform;
+            Speed = speed;
+        }
+
+        public void Move(float horizontal, float vertical, float deltaTime)
+        {
+            var speed = Speed * deltaTime;
+            _move.Set(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0f);
+            _transform.localPosition += _move;
+        }
+    }
+}
